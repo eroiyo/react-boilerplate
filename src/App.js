c895ffd7e36df125b9e5f403e86fea1e39690ac9
eroiyo <59938389+eroiyo@users.noreply.github.com>
2021-08-16 10:57:58 -0400

    fix linter 2

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ function App() {
           Edit
           <code>
             src/App.js
-          </code> and save to reload.
+          </code>
+          and save to reload.
         </p>
         <a
           className="App-link"
